1559eb1f6491e30f2cc159270c30e9988c988671
Midyan Hamdoun <socratides@gmail.com>
2020-06-04 20:56:21 +0300

    Add patients.js -WIP

--- routes/patients.js
+++ routes/patients.js
@@ -1,53 +1,100 @@
 const express = require('express');
-const connection = require('../connection/connection');
+const pool = require('../connection/connection');
 
 const router = express.Router();
 
 /* GET patients listing. */
 router.get('/', (req, res) => {
-  connection.connect();
-
-  connection.query('SELECT * FROM patients', (error, results) => {
-    if (error) throw error;
-    res.send(results);
+  pool.getConnection((err, connection) => {
+    connection.query('SELECT * FROM patients', (error, results) => {
+      connection.release();
+      if (error) throw error;
+      res.send(results);
+    });
   });
-
-  connection.end();
 });
 
 /* POST patient listing. */
 router.post('/', (req, res) => {
-  connection.connect();
+  pool.getConnection((err, connection) => {
+    const patientInfo = [
+      req.body.id,
+      req.body.first_name,
+      req.body.last_name,
+      new Date(2019, 12, 12),
+      req.body.telephone,
+      req.body.mobile,
+      req.body.amka,
+      req.body.afm,
+      req.body.comments,
+    ];
 
-  connection.query('', (error) => {
-    if (error) throw error;
-    res.send('POST method');
+    connection.query(
+      `
+    INSERT INTO patients(
+      id,
+      first_name,
+      last_name,
+      birth_date,
+      telephone,
+      mobile,
+      amka,
+      afm,
+      comments
+    ) VALUES (?)`,
+      [patientInfo],
+      (error) => {
+        connection.release();
+        if (error) throw error;
+        res.send('Entry added.');
+      },
+    );
   });
-
-  connection.end();
 });
 
 /* PUT patient listing. */
-router.put('/', (req, res) => {
-  connection.connect();
+router.put('/:id', (req, res) => {
+  pool.getConnection((err, connection) => {
+    // const patientInfo = [
+    //   `first_name=${req.body.first_name}`,
+    //   `last_name=${req.body.last_name}`,
+    //   `birth_date=${new Date(2019, 12, 12)}`,
+    //   `telephone=${req.body.telephone}`,
+    //   `mobile=${req.body.mobile}`,
+    //   `amka=${req.body.amka}`,
+    //   `afm=${req.body.afm}`,
+    //   `comments=${req.body.comments}`,
+    // ];
 
-  connection.query('', (error) => {
-    if (error) throw error;
-    res.send('PUT method');
+    connection.query(
+      `UPDATE patients SET first_name="${req.body.first_name}"
+      last_name="${req.body.last_name}"
+      birth_date=${new Date(2019, 12, 12)}
+      telephone=${req.body.telephone}
+      mobile=${req.body.mobile}
+      amka=${req.body.amka}
+      afm=${req.body.afm}
+      comments="${req.body.comments}" WHERE id=${req.params.id}`,
+      (error) => {
+        connection.release();
+        if (error) throw error;
+        res.send('Entry updated.');
+      },
+    );
   });
-
-  connection.end();
 });
 
-router.delete('/', (req, res) => {
-  connection.connect();
-
-  connection.query('', (error) => {
-    if (error) throw error;
-    res.send('DELETE method');
+router.delete('/:id', (req, res) => {
+  pool.getConnection((err, connection) => {
+    connection.query(
+      `DELETE FROM patients WHERE id=${req.params.id}`,
+      (error) => {
+        connection.release();
+        if (error) throw error;
+        res.send('Entry deleted.');
+      },
+    );
   });
-
-  connection.end();
 });
 
 module.exports = router;
