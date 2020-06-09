bd89647e31f7542e1fefb87ea4456dd66bf4c0a2
Midyan Hamdoun <socratides@gmail.com>
2020-06-09 18:36:30 +0300

    Add auto increment to id values

--- DB/frodid_app/frodid_app_users_patients.sql
+++ DB/frodid_app/frodid_app_users_patients.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `users_patients`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users_patients` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `patient_id` int NOT NULL,
   PRIMARY KEY (`id`),
--- DB/frodid_app/frodid_app_visits.sql
+++ DB/frodid_app/frodid_app_visits.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `visits`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `visits` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `comment` text NOT NULL,
   `start_time` time NOT NULL,
