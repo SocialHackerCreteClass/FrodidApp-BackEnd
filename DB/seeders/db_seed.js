fa367a743a40131cb09b990081e3e3ee0a3def9c
hookah96 <mak96grg@gmail.com>
2020-06-13 16:00:18 +0300

    add seed to DB pt2

--- DB/seeders/db_seed.js
+++ DB/seeders/db_seed.js
@@ -16,3 +16,145 @@ pool.getConnection((err, connection) => {
         if (err) throw err;
     }
 });
+
+pool.getConnection((err, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO users(
+                    first_name,
+                    last_name,
+                    email,
+                    password,
+                    birth_date,
+                    created_at,
+                    afm,
+                    amka,
+                    role_id,
+                    profession_id
+                ) VALUES (
+                    "${faker.name.firstName()}",
+                    "${faker.name.lastName()}",
+                    "${faker.internet.email()}",
+                    "${faker.internet.password({min: 8, max: 16})}",
+                    "${faker.date.past()}",
+                    "${faker.date.recent()}",
+                    "${faker.random.number({precision: 9})}",
+                    "${faker.random.number({precision: 11})}",
+                    "${faker.random.number(99)}",
+                    "${faker.random.number(99)}"
+                )`,
+                (error, results) => {
+                    console.log(results);
+                }
+            );
+        });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+pool.getConnection((error, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO professions (name) VALUES ("${faker.name.jobTitle()}")`,
+                (error, results) => {
+                    console.log(results);
+                }
+            );
+        });
+        
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+pool.getConnection((error, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO visits (
+                    date,
+                    comment,
+                    user_id
+                ) VALUES (
+                    "${faker.date.recent()}",
+                    "${faker.lorem.sentences()}",
+                    "${faker.random.number(99)}"
+                )`, 
+                (error, results) => {
+                    console.log(results);
+                }
+            );
+        });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+pool.getConnection((error, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO visits (
+                    start_time,
+                    end_time
+                ) VALUES (
+                    "12:45",
+                    "14:45"
+                ), (
+                    "9:50",
+                    "11:30"
+                ), (
+                    "13:15",
+                    "15:15"
+                ), (
+                    "17:20",
+                    "19:00"
+                )`,
+                (error, results) => {
+                    console.log(results);
+                }
+            )
+        });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+pool.getConnection((error, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO roles (name) VALUES ( admin ), ( professional )`,
+                (error, results) => {
+                    console.log(results);
+                }
+            );
+        });   
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
+
+pool.getConnection((error, connection) => {
+    try {
+        [...Array(100)].forEach(() => {
+            connection.query(
+                `INSERT INTO users_patients (
+                    user_id,
+                    patient_id
+                ) VALUES (
+                    "${faker.random.number(99)}",
+                    "${faker.random.number(99)}",
+                    )`,
+                (error, results) => {
+                    console.log(results);
+                }
+            );
+        });  
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+});
