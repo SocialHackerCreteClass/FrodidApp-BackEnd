69faca08549b0f3f6c2c49d293172671b25ac306
SalvadorCab <semerkhet40@hotmail.com>
2020-06-16 13:01:12 -0500

    Back to origin

--- connection/connection.js
+++ connection/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host: "ec2-18-185-118-132.eu-central-1.compute.amazonaws.com",
-  port: 3306,
-  user: "default",
-  password: "secret",
-  database: "default",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 });
 
