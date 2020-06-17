458ad1f9a7ace2d3b65754d98280536ee68b1434
hookah96 <mak96grg@gmail.com>
2020-06-17 15:13:17 +0300

    Update dates&hours with moment

--- routes/professions.js
+++ routes/professions.js
@@ -6,63 +6,72 @@ const router = express.Router();
 // Get Method
 router.get('/', (req, res) => {
   pool.getConnection((err, connection) => {
-    connection.query('SELECT * FROM professions', (error, results) => {
-      connection.release();
-      if (error) throw error;
-      res.send(results);
-    });
-    if (err) console.error(`Error with connection: ${err.message}`);
+    try {
+      connection.query('SELECT * FROM professions', (error, results) => {
+        connection.release();
+        res.send(results);
+      });
+    } catch (error) {
+      if (error) console.error(`Error: ${error.message}`);
+    }
   });
 });
 
 // Get with id Method
 router.get('/:id', (req, res) => {
   pool.getConnection((err, connection) => {
-    connection.query(
-      `SELECT * FROM professions WHERE id=${req.params.id}`,
-      (error, results) => {
-        connection.release();
-        if (error) throw error;
-        res.send(results);
-      },
-    );
-    if (err) console.error(`Error with connection: ${err.message}`);
+    try {
+      connection.query(
+        `SELECT * FROM professions WHERE id=${req.params.id}`,
+        (error, results) => {
+          connection.release();
+          res.send(results);
+        });
+    } catch (error) {
+      if (error) console.error(`Error: ${error.message}`);
+    }
   });
 });
 
 // Post Method
 router.post('/', (req, res) => {
   pool.getConnection((err, connection) => {
-    connection.query('', (error, results) => {
-      connection.release();
-      if (error) throw error;
-      res.send(results);
-    });
-    if (err) console.error(`Error with connection: ${err.message}`);
+    try {
+      connection.query('', (error, results) => {
+        connection.release();
+        res.send(results);
+      });
+    } catch (error) {
+      if (error) console.error(`Error: ${error.message}`);
+    }
   });
 });
 
 // Put Method
 router.put('/:id', (req, res) => {
   pool.getConnection((err, connection) => {
-    connection.query('', (error, results) => {
-      connection.release();
-      if (error) throw error;
-      res.send(results);
-    });
-    if (err) console.error(`Error with connection: ${err.message}`);
+    try {
+      connection.query('', (error, results) => {
+        connection.release();
+        res.send(results);
+      });
+    } catch (error) {
+      if (error) console.error(`Error: ${error.message}`);
+    }
   });
 });
 
 // Delete Method
 router.delete('/:id', (req, res) => {
   pool.getConnection((err, connection) => {
-    connection.query('', (error, results) => {
-      connection.release();
-      if (error) throw error;
-      res.send(results);
-    });
-    if (err) console.error(`Error with connection: ${err.message}`);
+    try {
+      connection.query('', (error, results) => {
+        connection.release();
+        res.send(results);
+      });
+    } catch (error) {
+      if (error) console.error(`Error: ${error.message}`);
+    }
   });
 });
 
