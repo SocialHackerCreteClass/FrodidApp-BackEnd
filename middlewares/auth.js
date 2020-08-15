941cde9c2304137cc4ca4a169e0080b93e274c51
SalvadorCab <semerkhet40@hotmail.com>
2020-08-15 04:01:04 -0500

    Eliminate console.log line

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,4 @@
 const auth = (req, res, next) => {
-    console.log("token: " + req.session);
     if(req.headers.authorization && req.headers.authorization === req.session.token){
         next();
     } else {
